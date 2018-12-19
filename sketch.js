f3fe91463026c8c0301ae9b7d8ae4bdef132a5fd
Hayden Shively <haydenshively@gmail.com>
2018-12-19 16:10:03 -0600

    Added images

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let contentHorizontalInset = 10;
+let contentVerticalInset = 20;
+var backgroundImage;
+
+function preload() {
+  backgroundImage = loadImage('assets/lane_lines.jpg')
+}
+
 function setup() {
   // put setup code here
+  createCanvas(windowWidth - contentHorizontalInset, windowHeight - contentVerticalInset)
+  background(0, 0, 0)
+  background(backgroundImage, [255])
 }
 
 function draw() {
   // put drawing code here
+  // ellipse(50, 50, 80, 80)
+  if (mouseIsPressed) {
+    fill(0)
+  }else {
+    fill(255)
+  }
+  ellipse(mouseX, mouseY, 80, 80)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth - contentHorizontalInset, windowHeight - contentVerticalInset)
 }
