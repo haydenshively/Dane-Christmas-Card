c5355f09117428073edd66abf57de811aa1cf95e
Hayden Shively <haydenshively@gmail.com>
2018-12-20 19:50:40 -0500

    Added results screen

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 let contentHorizontalInset = 10;
 let contentVerticalInset = 20;
-let swimmerSwitchFrameRate = 1000;
 
 var backgroundImage;
 var swimmerImage, swimmerImageLeft, swimmerImageRight, dolphinImage;
 
 var swimmerX, swimmerY, dolphinX, dolphinY;
 var swimmerIsLeft = true;
-var loopCount = 0;
+var swimmerSpeed = 10;
 
 
 function preload() {
@@ -41,25 +40,47 @@ function draw() {
     image(swimmerImage, swimmerX - swimmerImage.width/2, swimmerY - swimmerImage.height)
     image(dolphinImage, dolphinX - dolphinImage.width/2, dolphinY - dolphinImage.height)
 
-    dolphinY -= 2
+    dolphinY -= random(1, 3)
+  }else if (dolphinY - dolphinImage.height > 0) {
+    rectMode(CENTER)
+    fill(5, 5, 20)
+    rect(width/2, height/2, width/2, height/2, 15, 15, 15, 15);
+    fill(20, 255, 20)
+    textAlign(CENTER)
+    textStyle(BOLD)
+    textSize(32);
+    text('Dane wins! Merry Christmas!', width/2, height/2)
   }else {
-    text('You win!')
-    ellipse(mouseX, mouseY, 80, 80)
+    rectMode(CENTER)
+    fill(5, 5, 20)
+    rect(width/2, height/2, width/2, height/2, 15, 15, 15, 15);
+    fill(255, 20, 20)
+    textAlign(CENTER)
+    textStyle(BOLD)
+    textSize(32);
+    text('Dolphin wins. Shake device to try again.', width/2, height/2)
   }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth - contentHorizontalInset, windowHeight - contentVerticalInset)
-  if (swimmerY > height) {
-    swimmerY = height
-  }
+  resizeCanvas(windowWidth - contentHorizontalInset, windowHeight - contentVerticalInset);
+  if (swimmerY > height) {swimmerY = height;}
 }
 
 function touchMoved() {
-  return false
+  return false;
 }
 
 function touchStarted() {
-  swimmerIsLeft = !swimmerIsLeft
-  swimmerY -= 10
+  swimmerIsLeft = !swimmerIsLeft;
+  swimmerY -= swimmerSpeed;
+
+  if (abs(dolphinY - swimmerY) > 20) {swimmerSpeed += 1;}
+}
+
+function deviceShaken() {
+  swimmerY = height
+  dolphinY = height
+
+  swimmerSpeed = 10
 }
